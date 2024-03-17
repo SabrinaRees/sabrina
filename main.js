1ad346d429f5ca368ae9b7fe385b1031d6b2328e
JuliaRees <160231773+SabrinaRees@users.noreply.github.com>
2024-03-17 16:42:56 +0800

    test

--- main.js
+++ main.js
@@ -4,3 +4,24 @@ $(document).ready(function() {
       $('.hamburger-menu').toggleClass('active');
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const handleSubmit = (event) => {
+      event.preventDefault();
+
+      const myForm = event.target;
+      const formData = new FormData(myForm);
+
+      fetch("/", {
+          method: "POST",
+          body: formData,
+      })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+
+  document.querySelectorAll("form[data-netlify='true']").forEach(form => {
+      form.addEventListener("submit", handleSubmit);
+  });
+});
